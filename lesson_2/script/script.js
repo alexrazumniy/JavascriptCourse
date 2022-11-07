40141abd746bdf6afb56cdb4611011502ffc6f9c
= <=>
2022-11-07 20:42:40 +0200

    name

---
+++ lesson_2/script/script.js
@@ -0,0 +1,22 @@
+let userAge;
+  userAge = 44;
+let userName;
+  userName = 'John';
+let userSurname;
+  userSurname = 'Travolta';
+
+console.log(userName)
+
+let newUser;
+let purchaseList;
+let userOnline;
+let employeeSalary = '4000 USD';
+let mouseCursorCoordinates;
+
+console.log('Hello World!')
+
+let example = 'Моя зарплата составляет ';
+let equivalent = ', что равняется 160000 грн.';
+// mySalary = example + employeeSalary + equivalent
+
+console.log(example + employeeSalary + equivalent)
