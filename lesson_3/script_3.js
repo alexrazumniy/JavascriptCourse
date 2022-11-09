a99fe8d6570f7b274a3ad6d75c97100cbc736814
= <=>
2022-11-09 20:52:39 +0200

    task 3 in script_3.js is made

---
+++ lesson_3/script_3.js
@@ -0,0 +1,10 @@
+let number1 = Number(prompt('Введите число 1'));
+let number2 = Number(prompt('Введите число 2'));
+
+if (number1 > number2) {
+    alert(`Число ${number1} больше, чем число ${number2}`);
+} else if (number1 < number2) {
+    alert(`Число ${number1} меньше, чем число ${number2}`);
+} else {
+    alert(`Числа равны`);
+}
