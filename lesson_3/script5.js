603fad9f299c93cd30df54aaf3b86bde2ef05661
= <=>
2022-11-10 21:14:33 +0200

    hometask 5 added

--- lesson_3/script5.js
+++ lesson_3/script5.js
@@ -1,1 +1,11 @@
-const 
+const visitorAge = Number(prompt("Сколько вам лет?"));
+const isWithAdults = confirm("У вас есть разрешение взрослых?");
+
+if ((visitorAge >= 12 && visitorAge <= 18 && isWithAdults) ||
+    (visitorAge >= 60 && visitorAge <= 80 && isWithAdults)) {
+    alert("Доступ разрешен!");
+} else if (visitorAge > 18 && visitorAge < 60) {
+    alert("Доступ разрешен!");
+} else {
+    alert("Доступ запрещен!");
+}
