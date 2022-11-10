6f69773c518421272fa9d1b777144dcdd091737c
= <=>
2022-11-10 14:34:40 +0200

    files .html & .js created and arranged

--- lesson_3/script3.js
+++ lesson_3/script3.js
@@ -1,5 +1,5 @@
-let number1 = Number(prompt('Введите число 1'));
-let number2 = Number(prompt('Введите число 2'));
+const number1 = Number(prompt('Введите число 1'));
+const number2 = Number(prompt('Введите число 2'));
 
 if (number1 > number2) {
     alert(`Число ${number1} больше, чем число ${number2}`);
