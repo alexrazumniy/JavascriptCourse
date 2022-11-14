14a58f3071b90474d7048c0879a931edd4460707
= <=>
2022-11-14 19:32:02 +0200

    task 4 added

---
+++ lesson_4/script4.js
@@ -0,0 +1,13 @@
+// ЗАДАНИЕ 4
+
+// ---вариант 1---
+
+// function getSum() {
+//     const a = Number(prompt("Первое число:"));
+//     const b = Number(prompt("Второе число:"));
+    
+//     const sum = a + b;
+//     alert(`сумма чисел равна ` + sum);
+// }
+
+// getSum();
