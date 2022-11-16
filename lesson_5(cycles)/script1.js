978e638fc7b1fd8c68b26058264e52d057ad64d8
= <=>
2022-11-16 23:34:45 +0200

    task 1 added

---
+++ lesson_5(cycles)/script1.js
@@ -0,0 +1,49 @@
+// ---variant 1 WHILE---
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i++);
+// }
+
+// let i = 20;
+// while (i >= 10) {
+//     console.log(i--);
+// }
+
+
+// ---variant 2 DO...WHILE---
+
+// let i = 1;
+// do {
+//     console.log(i++);
+// } while (i <= 10);
+
+
+// let i = 20;
+// do {
+//     console.log(i--);
+// } while (i >= 10);
+
+
+// ---variant 3 FOR---
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// or another desicion:
+
+// for (let i = 1; i <= 10;) {
+//     console.log(i++);
+// }
+
+
+// for (let i = 20; i >= 10; i--) {
+//     console.log(i);
+// }
+
+// or another desicion:
+
+for (let i = 20; i >= 10;) {
+    console.log(i--);
+}
