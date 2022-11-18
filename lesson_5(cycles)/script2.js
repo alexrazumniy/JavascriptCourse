9e492999ee5e53c8d0f98bb6406ab66967d906a8
= <=>
2022-11-18 13:57:55 +0200

    task 2 corrected

--- lesson_5(cycles)/script2.js
+++ lesson_5(cycles)/script2.js
@@ -1,9 +1,21 @@
-for (let i = 2; i < 100; i++) {
-  for (let j = 2; j <= i; j++) {
-    if (i % j === 0 && j < i) {
-      break;
-    } else if (j === i) {
-      console.log(i);
-    }
+let n = 100;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
   }
+  console.log(i);
 }
+
+// второй вариант:
+
+// for (let i = 2; i < n; i++) {
+//   for (let j = 2; j <= i; j++) {
+//     if (i % j == 0 && j < i) {
+//       break;
+//     } else if (j === i) {
+//       console.log(i);
+//     }
+//   }
+// }
