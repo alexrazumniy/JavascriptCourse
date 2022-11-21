b52670ce217708f2cd541d4be6d0697b03c561e2
= <=>
2022-11-21 18:06:32 +0200

    task 3 corrected

--- lesson_5(cycles)/script3.js
+++ lesson_5(cycles)/script3.js
@@ -1,49 +1,13 @@
-// const LOGIN = "ALEX";
-// const PASSWORD = "q1w2e3";
-
-// const authorize = () => {
-//     let userLogin;
-//     let userPassword;
-//     let isAuthSuccess = false;
-//     let i = 2;
-
-//     while (i >= 0) {
-//         userLogin = prompt("Логин:");
-//         if (!userLogin) {
-//             alert("Введите логин!");
-//             continue;
-//         }
-
-//         userPassword = prompt("Пароль:");
-//         if (!userPassword) {
-//             alert("Введите пароль!");
-//             continue;
-//         }
-
-//         if (userLogin === LOGIN || userPassword === PASSWORD) {
-//             isAuthSuccess = true;
-//             alert("Добро пожаловать!");
-//             break;
-
-//         } else {
-//             alert("Данные неверны!");
-//         }
-//         alert(`Оставшееся количество попыток ввода - ${i--}`);
-//     }
-// }
-
-// authorize();
-
-
 const LOGIN = "ALEX";
 const PASSWORD = "q1w2e3";
 
 const authorize = () => {
     let userLogin;
     let userPassword;
-    let isAuthSuccess = false;
+    // let isAuthSuccess = false;
+    let i = 2;
 
-    for (let i = 2; i >= 0; i--) {
+    while (i >= 0) {
         userLogin = prompt("Логин:");
         if (!userLogin) {
             alert("Введите логин!");
@@ -57,15 +21,51 @@ const authorize = () => {
         }
 
         if (userLogin === LOGIN || userPassword === PASSWORD) {
-            isAuthSuccess = true;
+            // isAuthSuccess = true;
             alert("Добро пожаловать!");
             break;
 
         } else {
             alert("Данные неверны!");
         }
-        alert(`Оставшееся количество попыток ввода - ${i}`);
+        alert(`Оставшееся количество попыток ввода - ${i--}`);
     }
 }
 
 authorize();
+
+
+// const LOGIN = "ALEX";
+// const PASSWORD = "q1w2e3";
+
+// const authorize = () => {
+//     let userLogin;
+//     let userPassword;
+//     // let isAuthSuccess = false;
+
+//     for (let i = 2; i >= 0; i--) {
+//         userLogin = prompt("Логин:");
+//         if (!userLogin) {
+//             alert("Введите логин!");
+//             continue;
+//         }
+
+//         userPassword = prompt("Пароль:");
+//         if (!userPassword) {
+//             alert("Введите пароль!");
+//             continue;
+//         }
+
+//         if (userLogin === LOGIN || userPassword === PASSWORD) {
+//             // isAuthSuccess = true;
+//             alert("Добро пожаловать!");
+//             break;
+
+//         } else {
+//             alert("Данные неверны!");
+//         }
+//         alert(`Оставшееся количество попыток ввода - ${i}`);
+//     }
+// }
+
+// authorize();
