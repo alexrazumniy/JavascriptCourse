ea18607ca58d7b971b6a898c13862713f587e95e
= <=>
2022-11-21 12:27:50 +0200

    task 3 added

---
+++ lesson_5(cycles)/script3.js
@@ -0,0 +1,35 @@
+const LOGIN = "ALEX";
+const PASSWORD = "q1w2e3";
+
+const authorize = () => {
+    let userLogin;
+    let userPassword;
+    let isAuthSuccess = false;
+
+    for (let i = 2; i >= 0; i--) {
+        userLogin = prompt("Логин:");
+        if (!userLogin) {
+            alert("Введите логин!");
+            continue;
+        }
+
+        userPassword = prompt("Пароль:");
+        if (!userPassword) {
+            alert("Введите пароль!");
+            continue;
+        }
+
+        if (userLogin === LOGIN || userPassword === PASSWORD) {
+            isAuthSuccess = true;
+            alert("Добро пожаловать!");
+            break;
+
+        } else {
+            alert("Данные неверны!");
+        }
+        alert(`Оставшееся количество попыток ввода - ${i}`);
+    }
+    alert("У вас больше не осталось попыток!");
+}
+
+authorize();
