d3399c35848acfaedd02c96591a93da01fbca934
= <=>
2022-11-27 17:19:52 +0200

    task 2 added

--- lesson_6(numbers,strings)/script2.js
+++ lesson_6(numbers,strings)/script2.js
@@ -1,12 +1,22 @@
-let minNumber = 10;
-let maxNumber = 20;
+let min = 5;
+let max = 10;
 
 const getRandomInteger = () => {
-    minNumber = Math.ceil(minNumber);
-    maxNumber = Math.floor(maxNumber);
-    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const integerRandom = getRandomInteger(minNumber, maxNumber);
-console.log(`Случайное целое число в промежутке от ${minNumber} до ${maxNumber} равно ${integerRandom}`);
+const integerRandom = getRandomInteger(min, max);
+console.log(`Случайное целое число в промежутке от ${min} до ${max} равно ${integerRandom}`);
 
+
+// Вариант Ани
+
+// const getRandomNumber = (min, max) => {
+//     const random = min + Math.random(min) * (max + 1 - min);
+//     return Math.floor(random);
+// };
+
+// const resRandom = getRandomNumber(1, 5);
+// console.log(resRandom);
