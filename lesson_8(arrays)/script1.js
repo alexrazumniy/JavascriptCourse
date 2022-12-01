5ded1d27891272caad1b9aeedb2932c38ffc3fc0
= <=>
2022-12-01 15:25:39 +0200

    task 8.1 added

---
+++ lesson_8(arrays)/script1.js
@@ -0,0 +1,18 @@
+// countTrue([true, false, false, true, false]); // -> 2
+// countTrue([false, false, false, false]); // -> 0
+// countTrue([]); // -> 0
+
+const countTrue = (arr) => {
+    let value = 0;
+
+    for (let number of arr) {
+        console.log(number);
+        if (number === true) {
+            value += number
+        }
+    }
+    return value;
+}
+
+const result = countTrue([true, false, false, true, false]);
+console.log(`количество значений "true" в массиве равно ${result}`)
