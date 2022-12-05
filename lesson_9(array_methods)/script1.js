27df2cdc469713b1e6f2fa53d6bf91cb51dce408
= <=>
2022-12-05 20:37:44 +0200

    task 9.1 corrected

--- lesson_9(array_methods)/script1.js
+++ lesson_9(array_methods)/script1.js
@@ -55,9 +55,7 @@
 const numbers = [1, 10, 17, 24, 45];
 
 const averageValue = numbers.reduce((sum, value) => {
-    average = sum += value / numbers.length;
-        
-    return average
+    return sum += value / numbers.length;
 }, 0);
 
 console.log(averageValue)
