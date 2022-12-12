f33f18821e0b81f13afeb8421e1fcde9d115dd56
= <=>
2022-12-12 22:07:59 +0200

    task 10 added

--- lesson_10(library-practice)/script.js
+++ lesson_10(library-practice)/script.js
@@ -14,13 +14,12 @@ const books = [
     },
 ];
 
-const booksAvailable = [];
 const booksMissing = [];
 
 function library() {
     const action = prompt(`Какое действие вы хотите выполнить? Для выбора введите:
 1 - взять книгу, 2 - вернуть книгу, 3 - добавить книгу`);
-    
+
     switch (action) {
         case "1":
             takeBook();
@@ -44,14 +43,108 @@ function library() {
 }
 
 
-
-let Object = {}
-console.log(Object);
-
-
 const takeBook = () => {
-    
+
+    const availableBooksNames = books
+        .filter((book) => !book.isReading)
+        .map((book) => `- ${book.name}`)
+        .join("\n");
+
+    let desiredBookName = prompt(`Введите название или автора книги из списка: 
+${availableBooksNames}`);
+
+    if (!desiredBookName) {
+        alert(`Такой книги нет`)
+
+        return;
+    }
+
+    desiredBookName = desiredBookName.trim().toLowerCase();
+
+    const desiredBook = books.find((book) => {
+        return book.name.toLowerCase() === desiredBookName;
+    });
+
+    if (!desiredBook) {
+        alert(`Нет книги с таким названием`)
+
+        return;
+    }
+
+    if (desiredBook.isReading) {
+        alert(`Извините, эту книгу уже взяли почитать!`)
+
+        return;
+    }
+
+    desiredBook.isReading = true;
+
+    alert(`Отличный выбор! ID вашей книги - ${desiredBook.id}. Запомните его!`);
+
+    console.log(books);
+};
+
+
+const returnBook = () => {
+    const returningBookId = +prompt(`Введите ID книги, которую хотите вернуть`);
+
+    if (!returningBookId) {
+        alert(`Вы ничего не ввели!`);
+
+        return;
+    }
+
+    const currentBook = books.find((book) => book.id === returningBookId);
+
+    if (!currentBook) {
+        alert(`Неверный ID книги! Введите корректный ID`)
+
+        return;
+    }
+
+    if (!currentBook.isReading) {
+        alert(`Эта книга сейчас не читается`)
+
+        return;
+    }
+
+    currentBook.isReading = false;
+    alert(`Спасибо, приходите к нам еще! Вам понравилась книга ${currentBook.name}?`);
+};
+
+const addMyBook = () => {
+    const name = prompt(`Введите название книги, которую хотите добавить в библиотеку`);
+    const author = prompt(`Введите автора книги, которую хотите добавить в библиотеку`);
+
+    const newBook = {
+        name,
+        author,
+        isReading: false,
+        id: generateBookId (),
+    }
+
+    console.log(newBook);
+    books.push(newBook);
+    alert(`Спасибо, книга добавлена в библиотеку! ID книги ${newBook.id}?`);
 }
+
+function generateBookId () {
+    let isBookWithIdExists = true;
+    let generatedId;
+
+    do {
+        generatedId = Math.ceil(Math.random() * 1000);
+        // console.log(generatedId);
+        isBookWithIdExists = Boolean(books.find((book) => book.id === generateBookId))
+        // console.log(isBookWithIdExists);
+    } while (isBookWithIdExists);
+
+    return generatedId;
+}
+
+
+
+
 
 
 
