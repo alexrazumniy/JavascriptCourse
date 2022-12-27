3dc796589d2ebaabe5e909c6a022aee744e0f0b3
= <=>
2022-12-27 11:57:58 +0200

    task 13 added

--- lesson_13/script.js
+++ lesson_13/script.js
@@ -1,39 +1,10 @@
-const createTooltip = (element, tooltipText) => {
-    
-    targetElement = document.querySelector(`${element}`)
-    // targetElement = document.getElementById(`${element}`);
-    const targetCoords = targetElement.getBoundingClientRect();
+const d = document.querySelectorAll('.img')
+console.log(d);
 
-    tooltipElem = document.createElement('div');
-    tooltipElem.className = 'tooltip';
-    document.body.append(tooltipElem);
-    tooltipElem.innerText = tooltipText;
 
-    let left = (targetElement.offsetWidth - tooltipElem.offsetWidth) / 2 + targetCoords.left;
-    if (left < 0) left = 5; // чтоб не заходило за левый край окна и не сливалось с краем 
-
-    let top = targetCoords.top - tooltipElem.offsetHeight - 10;
-    
-    tooltipElem.style.left = left + 'px';
-    tooltipElem.style.top = top + 'px';
-
+const buttonNext = document.getElementById('btn_next');
+const handleParentClick = (event) => {
+    const clickedElement = event.target;
 }
 
-createTooltip('button', 'Here is the text tooltip')
-
-
-// console.dir(targetElement);
-// console.log(`tooltip.classList`, tooltipElem.classList);
-// console.log(targetCoords);    
-
-
-// Видимость подсказки
-
-// button.addEventListener('click', () => toggleTooltip(visible))
-
-// tooltipElem = document.querySelector('.tooltip');
-// const toggleTooltipVisibility = (element, className) => {
-//     element.classList.contains(className) ? element.classList.remove(className) : element.classList.add(className)
-// }
-
-// toggleTooltipVisibility(tooltipElem, 'visible')
+buttonNext.addEventListener('click', handleParentClick)
