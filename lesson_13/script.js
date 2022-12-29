f5c3cc3f622aa85f7ae4a35002037cf147e3c567
= <=>
2022-12-29 23:06:17 +0200

    task 13 performed

--- lesson_13/script.js
+++ lesson_13/script.js
@@ -2,81 +2,59 @@ const buttonNext = document.getElementById('btn_next');
 const buttonPrev = document.getElementById('btn_prev');
 
 const imageGallery = [...document.querySelectorAll('img')];
-console.log(imageGallery.length);
+let currentImage = []
 
-// let i = 0;
-// const showNextImage = (event) => {
-//     event.target = imageGallery[i].classList.replace('img', 'visible');
-//     if(event.target) {
-//         i += 1
-//     }
-//     console.log(i);
-// }
 
-let i = 0;
+let i = 1;
+let j = 0;
+
 const showNextImage = (event) => {
     for (let img of imageGallery) {
-        img = imageGallery[i].classList.replace('img', 'visible')
+        imageGallery[i].style.display = 'block';
     }
-    i += 1
+    i ++
+
+    for (let img of imageGallery) {
+        imageGallery[j].style.display = 'none';
+    }
+    j ++
+
     if (i === imageGallery.length) {
         i = 0
     }
-    console.log(i);
-}
 
-buttonNext.addEventListener('mousedown', showNextImage)
-
-let j = i + 1;
-const showCCCImage = (event) => {
-    for (let img of imageGallery) {
-        img = imageGallery[j].classList.replace('visible', 'img')
-    }
     if (j === imageGallery.length) {
-        j = 0
+        j = i - 1
     }
-    j += 1
-    console.log(j);
 }
 
-buttonNext.addEventListener('mouseup', showCCCImage)
+buttonNext.addEventListener('click', showNextImage)
+
+const showPrevImage = (event) => {
+    for (let img of imageGallery) {
+        imageGallery[i].style.display = 'block';
+    }
+    i ++
+
+    for (let img of imageGallery) {
+        imageGallery[j].style.display = 'none';
+    }
+    j ++
+
+    if (i === imageGallery.length) {
+        i = 0
+    }
+
+    if (j === imageGallery.length) {
+        j = i - 1
+    }
+}
+
+buttonPrev.addEventListener('click', showPrevImage)
+
 
 
 // buttonPrev.addEventListener('click', handleParentClick)
 // buttonNext.removeEventListener('click', showNextImage)
 
 
-
-
-/* цей код позначає картинки, для зручності розробки */
-// let i = 1;
-// for(let li of carousel.querySelectorAll('li')) {
-//   li.style.position = 'relative';
-//   li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-//   i++;
-// }
-
-// /* Конфігурація */
-// let width = 130; // ширина зображення
-// let count = 3; // видима кількість зображень
-
-// let list = carousel.querySelector('ul');
-// let listElems = carousel.querySelectorAll('li');
-
-// let position = 0; // початкова позиція каруселі
-
-// carousel.querySelector('.prev').onclick = function() {
-//   // зсув ліворуч
-//   position += width * count;
-//   // останній зсув вліво може бути не на 3, а на 2 або 1 елемент
-//   position = Math.min(position, 0)
-//   list.style.marginLeft = position + 'px';
-// };
-
-// carousel.querySelector('.next').onclick = function() {
-//   // зсув праворуч
-//   position -= width * count;
-//   // останнє пересування вправо може бути не на 3, а на 2 або 1 елемент
-//   position = Math.max(position, -width * (listElems.length - count));
-//   list.style.marginLeft = position + 'px';
-// };
