927c68310ee3ce9c601a1377102fd49896cbaaff
= <=>
2022-12-30 23:13:18 +0200

    task 13 performed

--- lesson_13(Events)/script.js
+++ lesson_13(Events)/script.js
@@ -2,59 +2,34 @@ const buttonNext = document.getElementById('btn_next');
 const buttonPrev = document.getElementById('btn_prev');
 const imageGallery = [...document.querySelectorAll('img')];
 
-let i = 1;
-let j = 0;
-let counter;
+let currentImageIndex = 0;
 
-const showNextImage = (event) => {
-    for (let img of imageGallery) {
-        imageGallery[i].style.display = 'block';
-    }
-    counter = i ++
-
-    for (let img of imageGallery) {
-        imageGallery[j].style.display = 'none';
-    }
-    counter = j ++
-
-    if (i === imageGallery.length) {
-        i = 0
-    }
-
-    if (j === imageGallery.length) {
-        j = i - 1
-    }
-    console.log(`counter`, counter);
-    return counter
+const slider = () => {
+	for (let i = 0; i < imageGallery.length; i ++) {
+		imageGallery[i].style.display = 'none';
+	}
+	imageGallery[currentImageIndex].style.display = 'block';	
 }
+slider();
 
+const showNextImage = () => {
+	
+	if (currentImageIndex + 1 === imageGallery.length) {
+		currentImageIndex = 0;
+	} else {
+		currentImageIndex ++
+	}
+	slider();
+}
 buttonNext.addEventListener('click', showNextImage)
 
-const showPrevImage = (event) => {
-    for (let img of imageGallery) {
-        imageGallery[i].style.display = 'block';
-    }
-    counter = imageGallery.length --
-
-    for (let img of imageGallery) {
-        imageGallery[j].style.display = 'none';
-    }
-    counter = imageGallery.length --
-
-    if (i === 0) {
-        i = imageGallery.length
-    }
-
-    if (j === imageGallery.length) {
-        j = i + 1
-    }
+const showPrevImage = () => {
+	
+	if (currentImageIndex - 1 === -1) {
+		currentImageIndex = imageGallery.length - 1;
+	} else {
+		currentImageIndex --
+	}
+	slider();
 }
-
 buttonPrev.addEventListener('click', showPrevImage)
-
-
-
-// buttonPrev.addEventListener('click', handleParentClick)
-// buttonNext.removeEventListener('click', showNextImage)
-
-
