a0ede78f7d6b63448f3a81d224ed0b47438fdbe2
= <=>
2023-02-01 17:53:55 +0200

    task 19.1 performed

---
+++ lesson_19(fetch_Async-Await-Promise_all_Try-catch)/script.js
@@ -0,0 +1,55 @@
+const container = document.getElementById("container");
+const preloader = document.getElementById("preloader");
+const countryElem = document.getElementById("country");
+const countryCapitalElem = document.getElementById("country_capital");
+const currencyElem = document.getElementById("currency");
+const flagImg = document.getElementById("flag");
+
+const BASE_URL = "https://ipapi.co/json";
+const COUNTRY_FLAG = "https://restcountries.com/v2/name";
+
+
+const showPreloader = (show) => {
+    if (show) {
+        preloader.style.display = "block";
+    } else {
+        preloader.style.display = "none";
+    }
+};
+
+const getCountry = async (url) => {
+
+    showPreloader(true);
+
+    const response = await fetch(url);
+    const countryRes = await response.json();
+    const {country_name: country, country_capital: capital, currency} = countryRes;
+    console.log(countryRes);
+        
+    getCountryFlag(COUNTRY_FLAG, country);
+    createCountryCard(country, capital, currency);
+
+}
+getCountry(BASE_URL)
+
+
+const getCountryFlag = async (url, countryInfo) => {
+
+    const response = await fetch(`${COUNTRY_FLAG}/${countryInfo}`);
+    const flag = await response.json();
+    const flagRes = renderFlag(flag[0].flag);    
+}
+
+const renderFlag = (flag) => {
+    flagImg.setAttribute('src', flag);
+}
+
+const createCountryCard = (country_name, country_capital, currency) => {
+    countryElem.innerText = country_name;
+    countryCapitalElem.innerText = country_capital;
+    currencyElem.innerText = currency;
+
+    showPreloader(false);
+};
+
+
