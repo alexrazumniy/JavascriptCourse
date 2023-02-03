4fe7ce938a15dcb34bc891ce727c7e8219f73375
= <=>
2023-02-03 17:28:35 +0200

    task 19.2 performed

--- lesson_19(fetch_Async-Await-Promise_all_Try-catch)/task 19.2/script.js
+++ lesson_19(fetch_Async-Await-Promise_all_Try-catch)/task 19.2/script.js
@@ -1,26 +1,107 @@
-const BASE_URL = "https://swapi.dev/api/films";
+const SWAPI_URL = "https://swapi.dev/api";
 
-// const showPreloader = (show) => {
-//     if (show) {
-//         preloader.style.display = "block";
-//     } else {
-//         preloader.style.display = "none";
-//     }
-// };
+const container = document.getElementById("container");
+const form = document.getElementById("form");
+const input = document.getElementById('input');
+const submitButton = document.getElementById('btn-submit');
+const errorMessage = document.getElementById('error_message')
+const preloader = document.getElementById("preloader");
 
-const getAllFilms = async (url) => {
+const showPreloader = (show) => {
+    if (show) {
+        preloader.style.display = "block";
+    } else {
+        preloader.style.display = "none";
+    }
+};
+
+// Отправка запроса на получение персонажа
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const charId = Number(input.value);
+    errorMessage.innerText = "";
+
+    if (charId < 1 || charId > 82 || !charId) {
+        errorMessage.innerText = "Некорректное значение!";
+        input.value = "";
+        return
+    }
+    getCharacter(charId);
+    submitButton.setAttribute("disabled", "");
+    submitButton.classList.remove("btn-submit");
+}
+form.addEventListener("submit", handleSubmit);
+
+// Получение персонажа
+const getCharacter = async (Id) => {
+
+    showPreloader(true);
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(`${SWAPI_URL}/people/${Id}`);
         if (response.status === 200 || response.status === 201) {
-            const filmsRes = await response.json();
-            console.log(filmsRes);
+            const charInfo = await response.json();
+            const { name: char_name, films: char_films } = charInfo;
+
+            renderCharCard(char_name, char_films);
+            showPreloader(false);
+            input.value = ""
         } else {
-            throw new Error(`An error has occurred. Error status ${response.status}`)
+            throw new Error(`Unable to get character from server. Error status "${response.status}"`)
         }
+
     } catch (err) {
-        let errorText = err.message
-        // alert(errorText)
+        let errorText = err.message;
+        alert(errorText);
+        showPreloader(false);
     }
 }
-getAllFilms(BASE_URL)
+
+// Рендеринг карточки персонажа
+const renderCharCard = (charName, filmsWithChar) => {
+
+    const characterName = document.createElement("h3");
+    characterName.innerText = charName;
+    characterName.classList.add("character_name");
+    container.append(characterName);
+
+    const filmBtn = document.createElement("button");
+    filmBtn.innerText = "Фильмы с участием";
+    filmBtn.classList.add("films_button");
+    container.append(filmBtn);
+
+    filmBtn.addEventListener("click", () => {
+        getFilmsList(filmsWithChar);
+        filmBtn.setAttribute("disabled", "");
+    })
+}
+
+// Получение и рендеринг списка фильмов
+const getFilmsList = async (urls) => {
+
+    showPreloader(true);
+
+    const requestFilms = urls.map((url) => fetch(url));
+
+    try {
+        const responses = await Promise.all(requestFilms);
+        const jsonResponses = responses.map((resp) => resp.json());
+        const filmsRes = await Promise.all(jsonResponses);
+
+        filmsRes.map((film) => {
+            const filmTitle = document.createElement("p");
+            filmTitle.classList.add("film_title");
+            filmTitle.innerText = film.title;
+            container.append(filmTitle);
+            showPreloader(false);
+        })
+
+    } catch (err) {
+        let errorText = err.message;
+        console.log(errorText);
+        alert(errorText);
+    }
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.add("btn-submit");
+}
