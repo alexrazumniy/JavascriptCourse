41cf8423e9d222571133dfaecef7a5e248a382f5
= <=>
2023-02-12 16:08:21 +0200

    task 20 requires correction

--- lesson_20(function-constructor-new_Prototype)/script.js
+++ lesson_20(function-constructor-new_Prototype)/script.js
@@ -1,72 +1,82 @@
-const container = document.getElementById("container");
-const preloader = document.getElementById("preloader");
-const countryElem = document.getElementById("country");
-const countryCapitalElem = document.getElementById("country_capital");
-const currencyElem = document.getElementById("currency");
-const flagImg = document.getElementById("flag");
-
-const BASE_URL = "https://ipapi.co/json";
-const COUNTRY_FLAG = "https://restcountries.com/v2/name";
-
-
-const showPreloader = (show) => {
-    if (show) {
-        preloader.style.display = "block";
-    } else {
-        preloader.style.display = "none";
-    }
-};
-
-const getCountry = async (url) => {
-
-    showPreloader(true);
-
-    try {
-        const response = await fetch(url);
-        if (response.status === 200 || response.status === 201) {
-            const countryRes = await response.json();
-            const { country_name: country, country_capital: capital, currency } = countryRes;
-
-            getCountryFlag(COUNTRY_FLAG, country);
-            createCountryCard(country, capital, currency);
-        } else {
-            throw new Error(`An error has occurred. Error status ${response.status}`)
-        }
-    } catch (err) {
-        let errorText = err.message
-        alert(errorText)
-    }
-
-}
-getCountry(BASE_URL)
-
-
-const getCountryFlag = async (url, countryName) => {
-
-    try {
-        const response = await fetch(`${COUNTRY_FLAG}/${countryName}`);
-        if (response.status === 200 || response.status === 201) {
-            const flag = await response.json();
-            renderFlag(flag[0].flag);
-        } else {
-            throw new Error(`An error has occurred. Error status ${response.status}`)
-        }
-    } catch (err) {
-        let errorText = err.message
-        alert(errorText)
-    }
+function PublicService({index, tariff}) {
+  this.index = index;
+  this.tariff = tariff;
+  console.log(this);
 }
 
-const renderFlag = (flag) => {
-    flagImg.setAttribute('src', flag);
+function addMeterReadings(index, value) {
+  // const {index, value} = options;
+  this.value = value;
+  PublicService.call(this, value);
 }
+const meterReadings = new PublicService( 'hotWater', 7 );
+console.log(meterReadings);
+// meterReadings.addMeterReadings(7, 3)
 
-const createCountryCard = (country_name, country_capital, currency) => {
-    countryElem.innerText = country_name;
-    countryCapitalElem.innerText = country_capital;
-    currencyElem.innerText = currency;
+PublicService.prototype.addMeterReadings = function (index, value) {
 
-    showPreloader(false);
-};
+    // const {index: value} = options;
+    for(option in tariffs) {
+      const indicator = document.createElement('p');
+      indicator.innerText = `${index} составляет ${value} куб.м`;
+      document.body.append(indicator);
+  
+    }
+    console.log(indicator);
+  }
 
 
+
+// const tariffs = {
+//   hotWater: 7,
+//   coldWater: 1,
+//   gas: 0.3,
+//   electricity: 1.6,
+// }
+
+
+
+
+
+// PublicService.prototype.addMeterReadings = function (index, value) {
+
+//   const {index: value} = options;
+//   for(option in tariffs) {
+//     const indicator = document.createElement('p');
+//     indicator.innerText = `${index} составляет ${value} куб.м`;
+//     document.body.append(indicator);
+
+//   // }
+//   // console.log(indicator);
+// }
+// }
+// const meterReadings = new PublicService({ tariffs });
+// console.log(meterReadings);
+// meterReadings.addMeterReadings({ tariffs })
+
+
+//   this.getSum = function (consumptionVolume, tariff) {
+//     this.consumptionVolume = consumptionVolume;
+//     this.tariff = tariff;
+//     const payment = consumptionVolume * tariff;
+//     console.log(payment);
+// }
+
+// const calcHotWater = new PublicService();
+// calcHotWater.addMeterReadings('Объем горячей воды', '100');
+// console.log(calcHotWater);
+
+// const calcColdWater = new PublicService();
+// calcColdWater.addMeterReadings('Объем холодной воды', '300')
+// console.log(calcColdWater);
+
+
+
+// const service = new PublicService();
+
+// service.addMeterReadings(100, "hotWater");
+// service.addMeterReadings(200, "coldWater");
+// service.deleteMeterReadings("coldWater");
+// service.addMeterReadings(300, "electricity");
+
+// const res = service.getSum();
