9021ce4e17e9539e4301e06874030d2198fb500c
= <=>
2023-02-14 17:46:28 +0200

    task 21

--- lesson_21(Class)/script.js
+++ lesson_21(Class)/script.js
@@ -1,68 +1,63 @@
-function PublicService(service) {
-  const { serviceIndex, value } = service;
-  this.serviceIndex = serviceIndex;
-  this.value = value;
+const SWAPI_URL = "https://swapi.dev/api";
+
+const container = document.getElementById("container");
+const form = document.getElementById("form");
+const input = document.getElementById('input');
+const submitButton = document.getElementById('btn-submit');
+// const errorMessage = document.getElementById('error_message')
+const preloader = document.getElementById("preloader");
+
+const showPreloader = (show) => {
+  if (show) {
+    preloader.style.display = "block";
+  } else {
+    preloader.style.display = "none";
+  }
+};
+
+class Card {
+  constructor() {
+    this.title = title;
+    this.text = text;
+    this.card = document.createElement("div");
+    this.render();
+  }
+
+  render() {
+    const cardContainer = document.createElement("div");
+    const closeCard = document.createElement("p");
+
+    cardContainer.classList.add("card_container");
+    closeCard.classList.add("close_card");
+
+    console.log(closeCard);
+    closeCard.innerText = "X";
+
+    this.card.append(cardContainer);
+  }
+
+  show() {
+    document.body.append(this.card);
+  }
+
+  hide() {
+    this.card.remove();
+  }
 }
 
-PublicService.prototype.addMeterReadings = function (tariff) {
-  this.tariff = tariff;
+class PlanetCard extends Card {
+  render() {
+    super.render();
+    const objectName = document.createElement("p");
+    objectName.classList.add("object_name");
 
-  const servicePay = this.value * this.tariff
-  console.log(servicePay);
-
-  const serviceName = document.createElement('p');
-  serviceName.innerText = `${this.serviceIndex}:  ${this.value}, tariff = ${this.tariff}, pay = ${servicePay}`
-  document.body.append(serviceName);
+    // objectName.innerText = 
+  }
 }
 
 
-// if (this.serviceIndex) {
-//   throw new Error(`Service ${this.serviceIndex} is unavailable`);
-// }
-
-
-PublicService.prototype.deleteMeterReadings = function (serviceIndex) {
-  console.log(serviceIndex);
+class API extends Card {
+  render() {
+    // const requestFilms
+  }
 }
-
-const service1 = new PublicService({ serviceIndex: 'hotWater', value: 100 });
-service1.addMeterReadings(7)
-console.log(service1);
-const service2 = new PublicService({ serviceIndex: 'coldWater', value: 200 });
-service2.addMeterReadings(1)
-const service3 = new PublicService({ serviceIndex: 'coldWater', value: 200 });
-service2.addMeterReadings(1)
-console.log(service2);
-
-
-
-
-
-
-
-
-//   this.getSum = function (consumptionVolume, tariff) {
-//     this.consumptionVolume = consumptionVolume;
-//     this.tariff = tariff;
-//     const payment = consumptionVolume * tariff;
-//     console.log(payment);
-// }
-
-// const calcHotWater = new PublicService();
-// calcHotWater.addMeterReadings('Объем горячей воды', '100');
-// console.log(calcHotWater);
-
-// const calcColdWater = new PublicService();
-// calcColdWater.addMeterReadings('Объем холодной воды', '300')
-// console.log(calcColdWater);
-
-
-
-// const service = new PublicService();
-
-// service.addMeterReadings(100, "hotWater");
-// service.addMeterReadings(200, "coldWater");
-// service.deleteMeterReadings("coldWater");
-// service.addMeterReadings(300, "electricity");
-
-// const res = service.getSum();
